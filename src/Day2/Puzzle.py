2ce59912efd46e6ccff1a0581302d1645305deb2
regina <regina.ochotzki@smarsteuer.de>
2023-12-02 09:17:15 +0100

    added parsing file into file data

--- src/Day2/Puzzle.py
+++ src/Day2/Puzzle.py
@@ -1,10 +1,33 @@
+def parseFile(filepath):
+    fileData = {}
 
+    file = open(filepath, 'r')
+
+    for line in file:
+        v = line.split(':')
+        gameId = v[0].strip().replace('Game ', '')
+        results = []
+        for r in [value.strip() for value in v[1].split(';')]:
+            results.append([value.strip().split(' ') for value in r.split(',')])
+
+        fileData[gameId] = results
+
+    file.close()
+
+    return fileData
 
 
 def calculatePuzzle(filepath):
     result = 0
+    red = 12
+    green = 13
+    blue = 14
+
+    fileData = parseFile(filepath)
+
+    for key, value in fileData.items():
+        print(key)
+        print(value)
 
 
     return result
-
-
--- tests/Day2/puzzleTest.py
+++ tests/Day2/puzzleTest.py
@@ -1,11 +1,20 @@
 import unittest
 
-from src.Day2.Puzzle import calculatePuzzle
+from src.Day2.Puzzle import calculatePuzzle, parseFile
 
 
 class TestPuzzle(unittest.TestCase):
 
-    def test_puzzlePart1(self):
-        result = calculatePuzzle('testInputPuzzle1.txt')
-        self.assertEqual(result, 0)
+    def test_parseFile(self):
+        result = parseFile('testInput2.txt')
 
+        expectedResult = \
+            {'1': [[['3', 'blue'], ['4', 'red']],
+                   [['1', 'red'], ['2', 'green'], ['6', 'blue']],
+                   [['2', 'green']]]
+             }
+        self.assertEqual(result, expectedResult)
+
+    def test_puzzle(self):
+        result = calculatePuzzle('testInput.txt')
+        self.assertEqual(result, 0)
