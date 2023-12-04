119525216568c875dfdc8cf4523fa3dc0d998b90
regina <regina.ochotzki@smarsteuer.de>
2023-12-04 11:36:05 +0100

    some refactoring

--- src/Day4/Puzzle.py
+++ src/Day4/Puzzle.py
@@ -1,35 +1,62 @@
 
 
-def parseFile(filepath):
-    fileData = []
+class Cards:
 
-    file = open(filepath, 'r')
+    def __init__(self):
+        self.cards = []
 
-    for line in file:
-        n = line.split(':')[1].strip().split('|')
+    def append(self, card):
+        self.cards.append(card)
+
+    def __str__(self):
+        return ' '.join([c.__str__() for c in self.cards])
+
+
+class Card:
+    def __init__(self, id, winningNumbers, numbers):
+        self.id = id
+        self.winningNumbers = winningNumbers
+        self.numbers = numbers
+
+    def __str__(self):
+        return f'numbers:{self.winningNumbers} winningNumbers:{self.winningNumbers}'
+
+    @staticmethod
+    def create(lineData):
+        cardData = lineData.split(':')
+        id = cardData[0].strip().split(' ')[1]
+
+        n = cardData[1].strip().split('|')
 
         winningNumbers = n[0].strip().split(' ')
         # filter out empty strings
         winningNumbers = [v for v in winningNumbers if v]
         numbers = n[1].split(' ')
         numbers = [v for v in numbers if v]
-        fileData.append({'winningNumbers': winningNumbers, 'numbers': numbers})
 
+        return Card(id, winningNumbers, numbers)
+
+
+
+
+def parseFile(filepath):
+    cards = Cards()
+
+    file = open(filepath, 'r')
+
+    for line in file:
+        cards.append(Card.create(line))
     file.close()
 
-    return fileData
+    return cards
 
 
 def calculatePoints(filename):
-
     cards = parseFile(filename)
-
     result = 0
-    for line in cards:
-        print(line)
-        winningNumbers = line['winningNumbers']
-        numbers = line['numbers']
-
+    for card in cards.cards:
+        winningNumbers =card.winningNumbers
+        numbers = card.numbers
         numberOfMatches = len(list(set(winningNumbers).intersection(numbers)))
         points = int(2**(numberOfMatches - 1))
         result += points
--- tests/Day4/puzzleTest.py
+++ tests/Day4/puzzleTest.py
@@ -7,10 +7,13 @@ class TestPuzzle(unittest.TestCase):
 
     def test_parseFile(self):
         result = parseFile('testInput1.txt')
-        expectedResult = [{'numbers': ['83', '86', '6', '31', '17', '9', '48', '53'],
-                           'winningNumbers': ['41', '48', '83', '86', '17']}]
-        self.assertEqual(result, expectedResult)
+        expectedResult = "numbers:['41', '48', '83', '86', '17'] winningNumbers:['41', '48', '83', '86', '17']"
+        self.assertEqual(result.__str__(), expectedResult)
 
     def test_calculatePoints(self):
         result = calculatePoints('testInput.txt')
         self.assertEqual(result, 13)
+
+    def test_calculatePointsInputProd(self):
+        result = calculatePoints('../../src/Day4/input.txt')
+        self.assertEqual(result, 25571)
